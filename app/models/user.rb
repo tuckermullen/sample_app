dc88bc7f42becce725c9ab09e8d2b84fd35a7c87
Tucker Mullen <mullentucker@gmail.com>
2016-02-17 21:34:13 +0000

    Finish log in/log out

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,4 +36,10 @@ class User < ActiveRecord::Base
   def forget
     update_attribute(:remember_digest, nil)
   end
+  
+  # Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
 end
