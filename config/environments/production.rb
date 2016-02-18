e9d9a82b2f4ff9f1c588d859aa2be671c00855c2
Tucker Mullen <mullentucker@gmail.com>
2016-02-18 23:08:52 +0000

    Fixed a production.rb typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,8 +62,9 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
+  
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery.method = :smtp
+  config.action_mailer.delivery_method = :smtp
   host = 'calm-refuge-38115.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
